dcb6a734f2941381510129999f3d5fe609010a26
Aryaveer Singh <96791182+AryaveerSingh2213@users.noreply.github.com>
2022-01-10 16:30:39 +0530

    Add files via upload

---
+++ Output_assignment_1.py
@@ -0,0 +1,22 @@
+Enter the first number  :55
+Enter the second number :65
+Enter the third number :75
+The first number is : 55
+The second number is : 65
+The third number is : 75
+The average of 55 , 65 and 75 is 65.0
+Tax Rate is :  20
+Standard Deduction is :  10000
+Additional Dependent Deduction is : 3000
+Enter Gross Income :60000.00
+Enter the Number of Dependents :4
+Taxable Income is : 38000.0
+Income Tax is  : 7600.0
+Student=[ SID , Name , Gender , Department , CGPA ]
+The student list is [21103004, 'Aryaveer', 'M', 'CSE', 7.9]
+Marks of five students are :
+List of marks before sorting is : [88, 90, 86, 89, 79]
+List of marks after sorting is : [79, 86, 88, 89, 90]
+Colour list is : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+ List of colours after removing the fourth element is : ['Red', 'Green', 'White', 'Pink', 'Yellow']
+ List of colours after replacing Black and Pink by Purple is : ['Red', 'Green', 'White', 'Purple', 'Yellow']
