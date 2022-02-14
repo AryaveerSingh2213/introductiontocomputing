86ea7b4719faf293ff5970fd62ef7565b0ea2981
Aryaveer Singh <96791182+AryaveerSingh2213@users.noreply.github.com>
2022-02-14 21:09:46 +0530

    Add files via upload

---
+++ Output_Assignment_3.py
@@ -0,0 +1,135 @@
+C:\Python310\python.exe C:/Users/aryav/PycharmProjects/firstProg/Assignment_3.py
+
+ Enter any string :Python is an object oriented programming language
+ The string entered by the user is : Python is an object oriented programming language
+ The frequency of each word is : {'Python': 1, 'is': 1, 'an': 1, 'object': 1, 'oriented': 1, 'programming': 1, 'language': 1}
+
+Enter month no.:2
+Enter year:2012
+Enter day:29
+
+Entered Date is: 29 / 2 / 2012
+Next Date is : 1/ 3 / 2012
+
+Enter the number of elements :5
+Enter the elements of the list :
+4
+6
+8
+7
+5
+The entered list of numbers is : [4, 6, 8, 7, 5]
+ The required list of tuples is : [(4, 16), (6, 36), (8, 64), (7, 49), (5, 25)]
+
+Enter your grade between 4 and 10 including them:10
+ Your Performance is:  Outstanding
+ Letter Grade is: A+
+
+The required pattern is :
+ ABCDEFGHIJK
+   ABCDEFGHI
+     ABCDEFG
+       ABCDE
+         ABC
+           A
+
+(a):Printing student details stored in the dictionary 
+
+Enter SID of a student:22103001
+Enter the name of the student:Zakhir Khan
+
+Do you want to enter more SIDs and Names ? ('y' or 'n'):y
+
+Enter SID of a student:22103002
+Enter the name of the student:Aarushi Jindal
+
+Do you want to enter more SIDs and Names ? ('y' or 'n'):y
+
+Enter SID of a student:22103003
+Enter the name of the student:Meenakshi Jha
+
+Do you want to enter more SIDs and Names ? ('y' or 'n'):y
+
+Enter SID of a student:22103004
+Enter the name of the student:Aryan Dutta
+
+Do you want to enter more SIDs and Names ? ('y' or 'n'):y
+
+Enter SID of a student:22103005
+Enter the name of the student:Priyesh Singh
+
+Do you want to enter more SIDs and Names ? ('y' or 'n'):n
+
+The entered dictionary is : {22103001: 'Zakhir Khan', 22103002: 'Aarushi Jindal', 22103003: 'Meenakshi Jha', 22103004: 'Aryan Dutta', 22103005: 'Priyesh Singh'}
+
+The entered list of SIDs is : [(22103001, 'Zakhir Khan'), (22103002, 'Aarushi Jindal'), (22103003, 'Meenakshi Jha'), (22103004, 'Aryan Dutta'), (22103005, 'Priyesh Singh')]
+
+(b):Sorting dictionary using student names'
+
+The dictionary we have is : {22103001: 'Zakhir Khan', 22103002: 'Aarushi Jindal', 22103003: 'Meenakshi Jha', 22103004: 'Aryan Dutta', 22103005: 'Priyesh Singh'}
+
+Exchanging the key-value pair , we get : {'Zakhir Khan': 22103001, 'Aarushi Jindal': 22103002, 'Meenakshi Jha': 22103003, 'Aryan Dutta': 22103004, 'Priyesh Singh': 22103005}
+
+The unsorted list of names is : [('Zakhir Khan', 22103001), ('Aarushi Jindal', 22103002), ('Meenakshi Jha', 22103003), ('Aryan Dutta', 22103004), ('Priyesh Singh', 22103005)]
+
+The list of name after sorting (alphabetically) is : [('Aarushi Jindal', 22103002), ('Aryan Dutta', 22103004), ('Meenakshi Jha', 22103003), ('Priyesh Singh', 22103005), ('Zakhir Khan', 22103001)]
+
+The sorted dictionary is :
+{'Aarushi Jindal': 22103002, 'Aryan Dutta': 22103004, 'Meenakshi Jha': 22103003, 'Priyesh Singh': 22103005, 'Zakhir Khan': 22103001}
+
+ The dictionary sorted on the basis of names is : {22103002: 'Aarushi Jindal', 22103004: 'Aryan Dutta', 22103003: 'Meenakshi Jha', 22103005: 'Priyesh Singh', 22103001: 'Zakhir 
+
+(c):Sorting dictionary using student SIDs 
+
+The list of SIDs is : [(22103001, 'Zakhir Khan'), (22103002, 'Aarushi Jindal'), (22103003, 'Meenakshi Jha'), (22103004, 'Aryan Dutta'), (22103005, 'Priyesh Singh')]
+
+The sorted list is : [(22103001, 'Zakhir Khan'), (22103002, 'Aarushi Jindal'), (22103003, 'Meenakshi Jha'), (22103004, 'Aryan Dutta'), (22103005, 'Priyesh Singh')]
+
+The dictionary sorted on the basis of SIDs is: {22103001: 'Zakhir Khan', 22103002: 'Aarushi Jindal', 22103003: 'Meenakshi Jha', 22103004: 'Aryan Dutta', 22103005: 'Priyesh Singh'}
+
+(d): Searching a student using SID
+
+ Enter one of the SIDs you entered before:22103003
+The name of the student with the SID  22103003  is: Meenakshi Jha
+
+Enter the first term :0
+Enter the second term :1
+[0, 1, 1]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2, 3]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2, 3, 5]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2, 3, 5, 8]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2, 3, 5, 8, 13]
+ Enter y to continue or n to stop here :y
+[0, 1, 1, 2, 3, 5, 8, 13, 21]
+ Enter y to continue or n to stop here :n
+
+The list having a Fibonacci Series is : [0, 1, 1, 2, 3, 5, 8, 13, 21]
+Average of the resultant Fibonacci series is :
+6.0
+
+Set1= {1, 2, 3, 4, 5}
+Set2= {8, 2, 4, 6}
+Set3= {1, 17, 5, 9, 13}
+
+(a)-------------------------
+Set1^Set2= {1, 3, 5, 6, 8}
+
+(b)-------------------------
+Set1^(Set2^Set3)= {3, 6, 8, 9, 13, 17}
+
+(c)-------------------------
+(Set1&Set2)|(Set2&Set3)|(Set1&Set3)= {1, 2, 4, 5}
+
+(d)-------------------------
+Set4-Set1= {6, 7, 8, 9, 10}
+
+(e)-------------------------
+Set4-(Set1|Set2|Set3)= {10, 7}
+
+----------END OF CODE----------
