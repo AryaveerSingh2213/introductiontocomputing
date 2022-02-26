0d4aae88f95d37a2d9f10abccb43a3b42b2ae377
Aryaveer Singh <96791182+AryaveerSingh2213@users.noreply.github.com>
2022-02-26 23:36:56 +0530

    Add files via upload

---
+++ Output_Assignment_4.py
@@ -0,0 +1,93 @@
+C:\Python310\python.exe C:/Users/aryav/PycharmProjects/firstProg/Assignment_4_cse_21103004.py
+
+ Transferring Disk  1 from rod A to rod C
+ Transferring Disk  2 from rod A to rod B
+ Transferring Disk  1 from rod C to rod B
+ Transferring Disk  3 from rod A to rod C
+ Transferring Disk  1 from rod B to rod A
+ Transferring Disk  2 from rod B to rod C
+ Transferring Disk  1 from rod A to rod C
+
+Using iteration......
+
+ Enter the number of rows :6
+
+     1  
+
+    1  1  
+
+   1  2  1  
+
+  1  3  3  1  
+
+ 1  4  6  4  1  
+
+1  5  10  10  5  1  
+
+Using recursion......
+
+ Enter the number of rows :7
+
+              1 
+
+             1 1 
+
+            1 2 1 
+
+           1 3 3 1 
+
+          1 4 6 4 1 
+
+         1 5 10 10 5 1 
+
+        1 6 15 20 15 6 1 
+
+Done!
+
+Enter the first number :50
+enter the second number :8
+
+The quotient is : 6
+The remainder is : 2
+
+ The resulting list is : [6, 2]
+
+ The function is callable : True
+
+ The first number is zero: False
+ The second number is zero: False
+ The quotient is zero : False
+ The remainder is zero : False
+
+ Adding (4, 5, 6) to the resulting list , we have :  [6, 2, 4, 5, 6]
+
+The values greater than four are : [6, 5, 6]
+
+ Converting the resulting list into a set ,we have :  {5, 6}
+
+ The required immutable set is : frozenset({5, 6})
+
+The required maximum value is : 6
+The required hash value is : 6
+
+Stuart
+22
+
+Destructor called,thus the object so created is destroyed.
+
+Mehak earns INR 40000
+Ashok earns INR 50000
+Viren earns INR 60000
+
+The updated salary of Mehak is :  70000
+
+The record of Viren has been deleted successfully.
+
+ Enter George's word :read
+ Enter Barbie's word :dear
+
+All the possible words are : ['read', 'erad', 'eard', 'eadr', 'raed', 'ared', 'aerd', 'aedr', 'rade', 'arde', 'adre', 'ader', 'reda', 'erda', 'edra', 'edar', 'rdea', 'drea', 'dera', 'dear', 'rdae', 'drae', 'dare', 'daer']
+
+If Barbie's word is present among all possible words, then their friendship is real.
+
+ Their friendship is REAL ! So they are real friends .
