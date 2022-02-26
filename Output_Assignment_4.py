a0a45c5f4e8883f6e0fdd4ad057491bfdd6a70c9
Aryaveer Singh <96791182+AryaveerSingh2213@users.noreply.github.com>
2022-02-26 23:45:00 +0530

    Update Output_Assignment_4.py

--- Output_Assignment_4.py
+++ Output_Assignment_4.py
@@ -1,4 +1,6 @@
 C:\Python310\python.exe C:/Users/aryav/PycharmProjects/firstProg/Assignment_4_cse_21103004.py
+  
+#Ques_1 :
 
  Transferring Disk  1 from rod A to rod C
  Transferring Disk  2 from rod A to rod B
@@ -7,6 +9,8 @@ C:\Python310\python.exe C:/Users/aryav/PycharmProjects/firstProg/Assignment_4_cs
  Transferring Disk  1 from rod B to rod A
  Transferring Disk  2 from rod B to rod C
  Transferring Disk  1 from rod A to rod C
+ 
+ #Ques_2(a) :
 
 Using iteration......
 
@@ -22,7 +26,9 @@ Using iteration......
 
  1  4  6  4  1  
 
-1  5  10  10  5  1  
+1  5  10  10  5  1 
+
+#Ques_2(b) :
 
 Using recursion......
 
@@ -44,6 +50,9 @@ Using recursion......
 
 Done!
 
+#Ques_3 :
+#(a)
+
 Enter the first number :50
 enter the second number :8
 
@@ -53,35 +62,55 @@ The remainder is : 2
  The resulting list is : [6, 2]
 
  The function is callable : True
+  
+  #(b)
 
  The first number is zero: False
  The second number is zero: False
  The quotient is zero : False
  The remainder is zero : False
+  
+  #(c)
 
  Adding (4, 5, 6) to the resulting list , we have :  [6, 2, 4, 5, 6]
 
 The values greater than four are : [6, 5, 6]
+ 
+ #(d) 
 
  Converting the resulting list into a set ,we have :  {5, 6}
+  
+ #(e)
 
  The required immutable set is : frozenset({5, 6})
+  
+ #(f)
 
 The required maximum value is : 6
 The required hash value is : 6
+ 
+ #Ques_4 :
 
 Stuart
 22
 
 Destructor called,thus the object so created is destroyed.
 
+#Ques_5 :
+
 Mehak earns INR 40000
 Ashok earns INR 50000
 Viren earns INR 60000
 
+#(a)
+
 The updated salary of Mehak is :  70000
+ 
+ #(b)
 
 The record of Viren has been deleted successfully.
+
+#Ques_6 :
 
  Enter George's word :read
  Enter Barbie's word :dear
